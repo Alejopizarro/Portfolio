430cc5871ce5146d89df58c420f61a51913506b1
Alejopizarro <alejomkt10@gmail.com>
2024-05-08 18:35:21 +0200

    clean project

--- pruebadev/styles/ejemplo.js
+++
@@ -1,9 +0,0 @@
-let text2 = document.getElementById("descJs").innerHTML
-    document.getElementById("descJs").innerHTML = text.replace(
-      "Lorem ipsum",
-      "JavaScript: Lenguaje de programacion para web que permite interactividad y dinamismo en paginas. Se ejecuta en el navegador.");      
-      
-    let text3 = document.getElementById("descCss").innerHTML;
-      document.getElementById("descCss").innerHTML = text.replace(
-      "Lorem ipsum",
-      "CSS -Hojas de Estilo en Cascada- Define la presentacion y estilo de paginas web, controla dise&ntilde;o, colores y formato para una mejor apariencia.");
